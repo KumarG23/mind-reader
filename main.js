c84e3ccd27bd3299dbe7eee3267ed39ba8408e7a
KumarG23 <sharma.neal@yahoo.com>
2024-04-03 21:41:20 -0400

    got it working

--- main.js
+++ main.js
@@ -1,51 +1,142 @@
- // variables
- let currentState = 1; // starting state
- let state = 'global'
- let symbols = ['!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?']
- // symbols for multiples of 9
 
- const init = () => {
-    document.getElementById('nextBtn').addEventListener('click', moveForward);
+ let currentPage = 0;// starting state
+ let symbols = ['!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?','!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?','!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?','!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?','!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?','!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?','!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?','!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?','!', '@', '#', '$', '%', '<', '^', '&', '*', '+', '?', '!'] // symbols for multiples of 9
+ let yourSymbol = null; // empty variable that will show a random symbol.
 
-    document.getElementById('resetBtn').addEventListener('click', reset);
+ // buttons variables that link to html
+ let mainText = document.getElementById('mainText');
+ let instructText = document.getElementById('instructText');
+ let nextBtn = document.getElementById('nextBtn');
+ let prevBtn = document.getElementById('prevBtn');
+ let resetBtn = document.getElementById('resetBtn');
+ let startBtn = document.getElementById('startBtn');
+ let revealBtn = document.getElementById('revealBtn');
 
-    document.getElementById('prevBtn').addEventListener('click', moveBack);
-
-    setState(currentState);
-
+ // function to send to next page and change state
+ function nextBtnFunc() {
+    currentPage++;
+    stateChange();
+ }
+ 
+ // function to reset game to first page and change state
+ function resetBtnFunc() {
+    currentPage = 0;
+    stateChange();
  }
 
- function setState() {
-    currentState = newState;
-    renderView(currentState);
-
+ // function to go to previous page and change state
+ function prevBtnFunc() {
+    currentPage--;
+    stateChange();
  }
 
-function renderView(state) {
-    const viewContainer = document.getElementById('viewContainer')
-    let html = '';
+
+
+ // click event listeners for each button
+ nextBtn.addEventListener('click', nextBtnFunc);
+ startBtn.addEventListener('click', nextBtnFunc);
+ prevBtn.addEventListener('click', prevBtnFunc);
+ resetBtn.addEventListener('click', resetBtnFunc);
+ revealBtn.addEventListener('click', nextBtnFunc);
+
+
+ // function to hide elements from certain pages in switch
+ function invisible(element) {
+    element.style.visibility = 'hidden';
+ }
+
+ // function to show elements on certain pages in switch
+function visible(element) {
+    element.style.visibility = 'visible'
 }
 
-switch (state) {
+function randomSymbol() {
+    let symbolString = "";
+    let randomNine = symbols[Math.floor(Math.random() * symbols.length)];
+
+    for (let i = 0; i < symbols.length; i++) {
+        if (i % 9 === 0) {
+            symbolString += i + " - " + randomNine + "<br>";
+        } else {
+            let random = symbols[Math.floor(Math.random() * symbols.length)];
+            symbolString += i + " - " + random + "<br>";
+        }
+    }
+    yourSymbol = randomNine;
+    return symbolString;
+}
+
+
+
+
+// state management function
+// switch case for each page + elements visible for each
+function stateChange(){
+switch (currentPage) {
+    case 0:
+        mainText.innerHTML="I can read your mind!";
+        invisible(nextBtn);
+        invisible(prevBtn);
+        invisible(instructText);
+        invisible(resetBtn);
+        visible(startBtn);
+        invisible(revealBtn);
+        break;
     case 1:
-        html = `<p>I can read your Mind!</p>`;
+        mainText.innerHTML="Pick a number from 01 - 99";
+        visible(nextBtn);
+        visible(prevBtn);
+        visible(instructText);
+        instructText.innerHTML="When you have your number click next"
+        visible(resetBtn);
+        invisible(startBtn);
+        invisible(revealBtn);
         break;
     case 2:
-        html = `<p>Pick a number from 01 - 99</p>`;
+        mainText.innerHTML="Add both digits together to get a new number";
+        visible(nextBtn);
+        visible(prevBtn);
+        visible(instructText);
+        instructText.innerHTML="Ex. 14 is 1 + 4 = 5 <br> click next to proceed";
+        visible(resetBtn);
+        invisible(startBtn);
+        invisible(revealBtn);
         break;
     case 3:
-        html = `<p>Add both digits together to get a new number</p`;
+        mainText.innerHTML="Subtract your new number from the original number";
+        visible(nextBtn);
+        visible(prevBtn);
+        visible(instructText);
+        instructText.innerHTML="Ex. 14 - 5 = 9 <br> click next to proceed";
+        visible(resetBtn);
+        invisible(startBtn);
+        invisible(revealBtn);
+        break;
     case 4:
-        html = `<p>Subtract your new number from the original number</p>`;
+        mainText.innerHTML= randomSymbol();
+        invisible(nextBtn);
+        visible(prevBtn);
+        visible(instructText);
+        instructText.innerHTML="Find your new number. <br> Note the symbol beside the number"
+        visible(resetBtn);
+        invisible(startBtn);
+        visible(revealBtn);
         break;
     case 5:
-        html = `<p>0 - ! 1 - @ 2 - # 3 -$ 4 - % 5 - < 6 - ^ 7 - & 8 - * 9 - ?</p>`;
+        mainText.innerHTML= yourSymbol;
+        invisible(nextBtn);
+        visible(prevBtn);
+        visible(instructText);
+        instructText.innerHTML="Your symbol is: <br>" + yourSymbol;
+        visible(resetBtn);
+        invisible(startBtn);
+        invisible(revealBtn);
         break;
-    case 6:
-    html = `<p>I read your Mind!</p>`
-    break;
+}
 }
 
-
-
-
+// function that runs when the page loads
+function init() {
+    resetBtnFunc();
+    stateChange();
+}
